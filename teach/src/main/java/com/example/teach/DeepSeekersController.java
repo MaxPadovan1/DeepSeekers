b365ddb000e37056547a14aa876d318eb8971a7b
Max P <maxP@email.com>
2025-04-24 16:24:10 +1000

    created maxs branch

--- teach/src/main/java/com/example/teach/DeepSeekersController.java
+++ teach/src/main/java/com/example/teach/DeepSeekersController.java
@@ -3,7 +3,7 @@ package com.example.teach;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class DeepSeekersController {
     @FXML
     private Label welcomeText;
 
