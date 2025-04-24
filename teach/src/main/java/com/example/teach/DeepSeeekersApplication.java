c41c25039760a3995c14bb14f179cc23adcc2089
Max P <maxP@email.com>
2025-04-24 16:31:54 +1000

    cleaned up project code

--- teach/src/main/java/com/example/teach/DeepSeeekersApplication.java
+++ teach/src/main/java/com/example/teach/DeepSeeekersApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class DeepSeeekersApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DeepSeeekersApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(DeepSeeekersApplication.class.getResource("DeepSeekers-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
+        stage.setTitle("DeepSeekers");
         stage.setScene(scene);
         stage.show();
     }
