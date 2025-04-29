cc5035908834aba3e152049bebadeacebf56cb53
Angela <n11996641@qut.edu.au>
2025-04-29 11:01:38 +1000

    created Dashboard-view.fxml and DashboardController class in a new branch for UI-Design and made the navigation from Loginpage to Dashboard possible.

--- teach/src/main/java/com/example/teach/controller/LoginPageController.java
+++ teach/src/main/java/com/example/teach/controller/LoginPageController.java
@@ -1,10 +1,17 @@
 package com.example.teach.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import  javafx.scene.Node;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
 
 
 public class LoginPageController {
@@ -15,23 +22,55 @@ public class LoginPageController {
     private PasswordField passwordField;
 
     @FXML
-    private Label messageLabel;
+    private Label errorLabel;
 
     @FXML
-    private Hyperlink fpassword;
+    private Hyperlink fPassword;
     @FXML
     private Label welcomeText;
     @FXML
     private Label welcomeText1;
 
     @FXML
-    private void handleLogin() {
+    private void handleLogin(ActionEvent event) {
         String userID = userId.getText();
         String password = passwordField.getText();
+
+        boolean loginSuccessful=true;
+
+        if(loginSuccessful)
+        {
+            openDashboard(event);
+        }
+        else
+        {
+            errorLabel.setText("Invalid userID or password!");
+        }
+    }
+
+    private void openDashboard(ActionEvent event)
+    {
+        try{
+            FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("/com/example/teach/Dashboard-view.fxml"));
+            Parent dashboardRoot= fxmlLoader.load();
+
+            Stage currentstage=(Stage) ((Node) event.getSource()).getScene().getWindow();
+            //get the new scene
+            Scene dashboardScene=new Scene(dashboardRoot);
+            currentstage.setTitle("Dashboard");
+            currentstage.setScene(dashboardScene);
+            currentstage.show();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
     @FXML
     private void handleSignUp() {
         String userID = userId.getText();
         String password = passwordField.getText();
     }
+
+
 }
