b025223f081c52d8ad26a45e239f4d59296001f2
Max P <maxP@email.com>
2025-04-30 19:51:01 +0800

    created basic SignUp-viewBasic.fxml and SignUpViewBasicController

--- teach/src/main/java/com/example/teach/controller/LoginPageController.java
+++ teach/src/main/java/com/example/teach/controller/LoginPageController.java
@@ -25,17 +25,7 @@ public class LoginPageController {
     @FXML private void handleLogin(ActionEvent event) {
         String userID = userId.getText();
         String password = passwordField.getText();
-
-        boolean loginSuccessful=true;
-
-        if(loginSuccessful)
-        {
-            openDashboard(event);
-        }
-        else
-        {
-            errorLabel.setText("Invalid userID or password!");
-        }
+        //TODO: actually login
     }
 
     private void openDashboard(ActionEvent event)
@@ -56,8 +46,21 @@ public class LoginPageController {
             e.printStackTrace();
         }
     }
-    @FXML private void handleSignUp() {
-        String userID = userId.getText();
-        String password = passwordField.getText();
+    @FXML private void handleSignUp(ActionEvent event)
+    {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/teach/SignUp-viewBasic.fxml"));
+            Parent signUpRoot = fxmlLoader.load();
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene signUpScene = new Scene(signUpRoot);
+            currentStage.setScene(signUpScene);
+            currentStage.show();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            errorLabel.setText("Unable to load Sign Up page. Please try again later.");
+       }
     }
 }
---
+++ teach/src/main/java/com/example/teach/controller/SignUpViewBasicController.java
@@ -0,0 +1,11 @@
+package com.example.teach.controller;
+
+import javafx.event.ActionEvent;
+
+public class SignUpViewBasicController {
+    public void handleSignUp(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+}
