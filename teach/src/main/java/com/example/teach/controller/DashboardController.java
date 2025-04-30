9f6d7a198d09f53e4bec932e3fc86cbe350bbf9b
Angela <n11996641@qut.edu.au>
2025-04-30 19:16:57 +1000

    created dashboard and added UI elements

--- teach/src/main/java/com/example/teach/controller/DashboardController.java
+++ teach/src/main/java/com/example/teach/controller/DashboardController.java
@@ -1,4 +1,26 @@
 package com.example.teach.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class DashboardController {
+    @FXML
+    private VBox drawer;
+
+    @FXML
+    private void toggleDrawer() {
+        boolean isVisible = drawer.isVisible();
+        drawer.setVisible(!isVisible);
+        drawer.setManaged(!isVisible);
+    }
+    @FXML
+    private void goBack() {
+        System.out.println("Back button clicked!");
+    }
+
+    @FXML
+    private void showNotifications() {
+        System.out.println("Notification button clicked!");
+    }
+
 }
