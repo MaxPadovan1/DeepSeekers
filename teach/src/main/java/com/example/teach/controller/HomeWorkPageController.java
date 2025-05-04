538870c6aba33fe61fc6f79074ab6763fcdff9e0
navaneethnsr <navaneethnsr@gmail.com>
2025-05-04 10:33:56 +1000

    fixed some bugs.

--- teach/src/main/java/com/example/teach/controller/HomeWorkPageController.java
+++
@@ -1,8 +0,0 @@
-package com.example.teach.controller;
-
-import javafx.event.ActionEvent;
-
-public class HomeWorkPageController {
-    public void handleLinkClick(ActionEvent actionEvent) {
-    }
-}
